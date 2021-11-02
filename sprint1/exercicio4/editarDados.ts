7618b28304df502fe01bc022f3cb246e8a58b1d1
Jean <jeandmalves@gmail.com>
2021-11-02 16:29:52 -0300

    Refatorado Botão Editar

--- sprint1/exercicio4/editarDados.ts
+++ sprint1/exercicio4/editarDados.ts
@@ -1,6 +1,6 @@
 const botao_editar: HTMLInputElement = document.getElementById('editar-conteudo') as HTMLInputElement
 
-botao_editar.addEventListener('click', function () {
+botao_editar.addEventListener('click', function (event) {
     
     event.preventDefault();
 
@@ -15,8 +15,12 @@ botao_editar.addEventListener('click', function () {
         if(dados_linha[0].innerHTML == id.value){
             dados_linha[1].innerHTML = name.value
             dados_linha[2].innerHTML = bio.value
+            return
         }
     }
+
+    adiciona_dados_tabela(id, name, bio)
+
 })
 
 const pega_dados = ():Array<HTMLInputElement> => {
@@ -27,3 +31,31 @@ const pega_dados = ():Array<HTMLInputElement> => {
     return [id, name, bio]
 
 }
+
+const adiciona_dados_tabela = (id:HTMLInputElement, name:HTMLInputElement, bio:HTMLInputElement) => {
+    let tabela = document.getElementById('dados_tabela')
+
+    let elemento = montaTr(id, name, bio)
+    tabela.appendChild(elemento)
+}
+
+const montaTd = (dados:any) => {
+
+    let td = document.createElement('td')
+    td.textContent = dados
+    td.classList.add('tabela-conteudo')
+    
+    return td
+}
+
+const montaTr = (id:HTMLInputElement, name:HTMLInputElement, bio:HTMLInputElement):HTMLTableRowElement => {
+    let linha = document.createElement('tr')
+    linha.id = id.value
+    
+    linha.appendChild(montaTd(id.value))
+    linha.appendChild(montaTd(name.value))
+    linha.appendChild(montaTd(bio.value))
+
+    return linha
+}
+
--- sprint1/exercicio4/js/editarDados.js
+++ sprint1/exercicio4/js/editarDados.js
@@ -1,5 +1,5 @@
 const botao_editar = document.getElementById('editar-conteudo');
-botao_editar.addEventListener('click', function () {
+botao_editar.addEventListener('click', function (event) {
     event.preventDefault();
     let [id, name, bio] = pega_dados();
     let dados_tabela = document.getElementById('dados_tabela').children;
@@ -8,13 +8,34 @@ botao_editar.addEventListener('click', function () {
         if (dados_linha[0].innerHTML == id.value) {
             dados_linha[1].innerHTML = name.value;
             dados_linha[2].innerHTML = bio.value;
+            return;
         }
     }
+    adiciona_dados_tabela(id, name, bio);
 });
 const pega_dados = () => {
     let id = document.getElementById('id');
     let name = document.getElementById('name');
     let bio = document.getElementById('bio');
     return [id, name, bio];
+};
+const adiciona_dados_tabela = (id, name, bio) => {
+    let tabela = document.getElementById('dados_tabela');
+    let elemento = montaTr(id, name, bio);
+    tabela.appendChild(elemento);
+};
+const montaTd = (dados) => {
+    let td = document.createElement('td');
+    td.textContent = dados;
+    td.classList.add('tabela-conteudo');
+    return td;
+};
+const montaTr = (id, name, bio) => {
+    let linha = document.createElement('tr');
+    linha.id = id.value;
+    linha.appendChild(montaTd(id.value));
+    linha.appendChild(montaTd(name.value));
+    linha.appendChild(montaTd(bio.value));
+    return linha;
 };
 //# sourceMappingURL=editarDados.js.map
